30371eaaafce3c0c49c4db7fdc31065a0969a034
Austin Jackson <austinjckson@gmail.com>
2019-01-26 21:20:53 -0600

    login post and things testing

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var parseurl = require('parseurl')
 var session = require('express-session')
+var bodyParser = require('body-parser')
 var router = require('./routes/api');
 const port = 3000
 
@@ -11,6 +12,10 @@ app.use(session({
     resave: false,
     saveUninitialized: true
 }))
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 app.use('/api', router);
 
--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,17 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
 var session = require('express-session');
+const sha256 = require('simple-sha256')
+require('dotenv').config()
+var knex = require('knex')({
+    client: 'mysql',
+    connection: {
+        host : process.env.DB_HOST,
+        user : process.env.DB_USER,
+        password : process.env.DB_PASS,
+        database : process.env.DB_NAME
+    }
+});
 
 router.get('/', (req, res, next) => {
     //req.session.last_update = String(new Date())
@@ -9,7 +20,28 @@ router.get('/', (req, res, next) => {
 })
 
 router.get('/echo', (req, res) => {
-    res.json(req.query)
+    res.json(req.session)
+})
+
+router.post('/login', (req, res) => {
+    console.log(req.body)
+    if(req.body.username && typeof req.body.username === 'string' && req.body.password && typeof req.body.password === 'string') {
+        knex.select('*').from('users').then(users => {
+            for(let i = 0; i < users.length; i++) {
+                if(req.body.username.trim() === users[i]['username'].trim()) {
+                    if(sha256.sync(req.body.password) === users[i]['password']) {
+                        console.log('login good')
+                        req.session.username = users[i]['username']
+                        req.session.logged_in = true
+                        res.json('login good')
+                    }
+                }
+            }
+        })
+    }
+    else {
+        res.json("login bad")
+    }
 })
 
 router.get('/register', (req, res) => {
@@ -23,3 +55,6 @@ router.get('/update', (req, res, next) => {
 })
 
 module.exports = router
+
+
+process.on('unhandledRejection', r => console.error('unhandledRejection: ',r.stack,'\n',r));
