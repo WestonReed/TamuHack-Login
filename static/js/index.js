57f90565641f15930234fbcdf88838e7da065827
Austin Jackson <themacphage@gmail.com>
2019-01-27 11:59:59 +0000

    login page worksgit add --allgit add --allgit add --allgit add --allgit add --allgit add --all

--- static/js/index.js
+++ static/js/index.js
@@ -5,29 +5,44 @@
         }
         else {
             Swal.fire({
-                title: 'Are you sure?',
-                text: 'You will not be able to recover this imaginary file!',
+                title: 'Here\'s how this works',
+                html: 'To confirm that you really do have access to the Ethereum address that your browser claims you have, we\'re going to ask Metamask to verify a set of special <span style="font-family: monospace;">code words</span> we have in mind.<br><br>This requires your intervention, so please confirm your identity by signing the following message for us.',
                 type: 'info',
                 showCancelButton: true,
-                confirmButtonText: 'Yes, delete it!',
-                cancelButtonText: 'No, keep it'
-              }).then((result) => {
-                if (result.value) {
-                  Swal.fire(
-                    'Deleted!',
-                    'Your imaginary file has been deleted.',
-                    'success'
-                  )
-                // For more information about handling dismissals please visit
-                // https://sweetalert2.github.io/#handling-dismissals
-                } else if (result.dismiss === Swal.DismissReason.cancel) {
-                  Swal.fire(
-                    'Cancelled',
-                    'Your imaginary file is safe :)',
-                    'error'
-                  )
-                }
-              })
+                confirmButtonText: 'Open Metamask'
+            }).then((result) => {
+                fetch('/api/nonce', {
+                    method: 'POST',
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({
+                        public_address: web3.eth.coinbase
+                    })
+                })
+                .then(res => res.json())
+                .then(data => {
+                    if(data.status === 'success') {
+                        web3.personal.sign(data.payload.nonce, web3.eth.coinbase,(err, signature) => {
+                            fetch('/api/verify', {
+                                method: 'POST',
+                                headers: { "Content-Type": "application/json" },
+                                body: JSON.stringify({
+                                    public_address: web3.eth.coinbase,
+                                    signature: signature
+                                })
+                            })
+                            .then(res => res.json())
+                            .then(data => {
+                                if(data.status === 'success') {
+                                    document.location.href = '/private'
+                                }
+                            })
+                        });
+                    }
+                    else {
+                        Swal.fire('Something went wrong.', JSON.stringify(data), 'error')
+                    }
+                })
+            })
         }
     })
 })();
